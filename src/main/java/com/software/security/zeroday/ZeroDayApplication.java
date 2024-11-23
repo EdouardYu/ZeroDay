3cec0703f16912479a07cd7f53e7479c9fd37b2b
EdouardY <yuedouard@gmail.com>
2024-11-23 04:16:50 +0100

    init

---
+++ src/main/java/com/software/security/zeroday/ZeroDayApplication.java
@@ -0,0 +1,13 @@
+package com.software.security.zeroday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/software/security/zeroday/ZeroDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.security.zeroday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
